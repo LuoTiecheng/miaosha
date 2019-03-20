850a1a36bc64b0ea18ef02e8688c3ffc74ae0102
Luo Tiecheng <luotiecheng001@ke.com>
2019-03-20 17:44:34 +0800

    super

--- src/main/java/com/ltc/miaosha/controller/GoodsController.java
+++ src/main/java/com/ltc/miaosha/controller/GoodsController.java
@@ -1,12 +1,8 @@
 package com.ltc.miaosha.controller;
 
-
 import java.util.List;
 
-import com.ltc.miaosha.redis.GoodsKey;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +13,6 @@ import com.ltc.miaosha.redis.RedisService;
 import com.ltc.miaosha.service.GoodsService;
 import com.ltc.miaosha.service.MiaoshaUserService;
 import com.ltc.miaosha.vo.GoodsVo;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.spring4.context.SpringWebContext;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/goods")
@@ -37,44 +27,19 @@ public class GoodsController {
 	@Autowired
 	GoodsService goodsService;
 
-	@Autowired
-	ThymeleafViewResolver thymeleafViewResolver;
-
-	@Autowired
-	ApplicationContext applicationContext;
-
-    @RequestMapping(value = "/to_list", produces="text/html")
-	@ResponseBody
-    public String list(HttpServletRequest request, HttpServletResponse response, Model model, MiaoshaUser user) {
+    @RequestMapping("/to_list")
+    public String list(Model model,MiaoshaUser user) {
     	model.addAttribute("user", user);
-
-		String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
-		if(!StringUtils.isEmpty(html)) {
-			return html;
-		}
     	//查询商品列表
     	List<GoodsVo> goodsList = goodsService.listGoodsVo();
     	model.addAttribute("goodsList", goodsList);
-		SpringWebContext ctx = new SpringWebContext(request,response,
-				request.getServletContext(),request.getLocale(), model.asMap(), applicationContext );
-		//手动渲染
-		 html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
-		if(!StringUtils.isEmpty(html)) {
-			redisService.set(GoodsKey.getGoodsList, "", html);
-		}
-		return html;
+        return "goods_list";
     }
 
-    @RequestMapping(value = "/to_detail/{goodsId}", produces="text/html")
-    public String detail(HttpServletRequest request, HttpServletResponse response,Model model,MiaoshaUser user,
+    @RequestMapping("/to_detail/{goodsId}")
+    public String detail(Model model,MiaoshaUser user,
     		@PathVariable("goodsId")long goodsId) {
     	model.addAttribute("user", user);
-
-		//取缓存
-		String html = redisService.get(GoodsKey.getGoodsDetail, ""+goodsId, String.class);
-		if(!StringUtils.isEmpty(html)) {
-			return html;
-		}
 
     	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
     	model.addAttribute("goods", goods);
@@ -84,10 +49,12 @@ public class GoodsController {
     	long now = System.currentTimeMillis();
     	int miaoshaStatus = 0;
     	int remainSeconds = 0;
-    	if(now < startAt ) {//秒杀还没开始，倒计时
+		//秒杀还没开始，倒计时
+    	if(now < startAt ) {
     		miaoshaStatus = 0;
     		remainSeconds = (int)((startAt - now )/1000);
-    	}else  if(now > endAt){//秒杀已经结束
+    	}else  if(now > endAt){
+    		//秒杀已经结束
     		miaoshaStatus = 2;
     		remainSeconds = -1;
     	}else {//秒杀进行中
@@ -96,14 +63,7 @@ public class GoodsController {
     	}
     	model.addAttribute("miaoshaStatus", miaoshaStatus);
     	model.addAttribute("remainSeconds", remainSeconds);
-//        return "goods_detail";
-		SpringWebContext ctx = new SpringWebContext(request,response,
-				request.getServletContext(),request.getLocale(), model.asMap(), applicationContext );
-		html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
-		if(!StringUtils.isEmpty(html)) {
-			redisService.set(GoodsKey.getGoodsDetail, ""+goodsId, html);
-		}
-		return html;
+        return "goods_detail";
     }
     
 }
